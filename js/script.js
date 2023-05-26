20b52b205ebe088ce84df9141ecbd46b7263cf0b
Natqueen <natasha.paola13@gmail.com>
2023-05-25 21:33:31 -0300

    alterações de velocidade e looping de reinicio

--- js/script.js
+++ js/script.js
@@ -1,43 +1,96 @@
 const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-
-
+const gameBoard = document.querySelector('.game-board');
+let isGameOver = false;
+let speed = 2; // Velocidade inicial do Mario é 2
+let loop;
+let pipeInterval;
 
 const jump = () => {
+  if (isGameOver) {
+    return;
+  }
+
   mario.classList.add('jump');
 
   setTimeout(() => {
     mario.classList.remove('jump');
-
   }, 500);
+};
 
-}
+const createPipe = () => {
+  const pipe = document.createElement('img');
+  pipe.src = './js/images/pipe.png';
+  pipe.classList.add('pipe');
+  gameBoard.appendChild(pipe);
 
-const loop = setInterval(() => {
+  return pipe;
+};
 
-    console.log('loop')
+const movePipes = () => {
+  const pipes = document.querySelectorAll('.pipe');
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-    
+  pipes.forEach((pipe) => {
+    const pipePosition = pipe.offsetLeft - speed;
 
-    if (pipePosition < 120 && pipePosition > 0 && marioPosition < 80) {
+    if (pipePosition < -80) {
+      pipe.remove();
+    } else {
+      pipe.style.left = `${pipePosition}px`;
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+      const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
-
-        mario.src = "./js/images/lost.png";
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
-
-        clearInterval(loop);
-
-
+      if (pipePosition < 120 && pipePosition > 0 && marioPosition < 80) {
+        gameOver();
+      }
     }
-    
-}, 10);
+  });
+};
+
+const gameLoop = () => {
+  if (isGameOver) {
+    return;
+  }
+
+  const pipe = createPipe();
+  pipeInterval = setInterval(movePipes, 10);
+};
+
+const gameOver = () => {
+  isGameOver = true;
+  clearInterval(loop);
+  clearInterval(pipeInterval);
+
+  const pipes = document.querySelectorAll('.pipe');
+  pipes.forEach((pipe) => pipe.remove());
+
+  mario.classList.remove('jump');
+  mario.src = "./js/images/lost.png";
+  mario.style.width = '75px';
+  mario.style.marginLeft = '50px';
+
+  const restartButton = document.createElement('button');
+  restartButton.textContent = 'Reiniciar';
+  restartButton.addEventListener('click', restartGame);
+
+  const restartContainer = document.createElement('div');
+  restartContainer.classList.add('restart-container');
+  restartContainer.appendChild(restartButton);
+  gameBoard.appendChild(restartContainer);
+};
+
+const restartGame = () => {
+  isGameOver = false;
+  speed = 2; // Reinicia a velocidade do Mario para 2
+  const restartContainer = document.querySelector('.restart-container');
+  restartContainer.remove();
+
+  mario.src = "./js/images/mario.gif";
+  mario.style.width = '150px';
+  mario.style.marginLeft = '0';
+
+  loop = setInterval(gameLoop, 2000);
+};
 
 document.addEventListener('keydown', jump);
+
+loop = setInterval(gameLoop, 2000);
